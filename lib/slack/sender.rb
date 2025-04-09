24f0f8855ed6f717c3305a9d1a891fe081a42c66
Gilberto Bandeira <bandeirasbeto@gmail.com>
2025-04-08 22:06:43 -0300

    refactor sender service to improve SRP

--- lib/slack/sender.rb
+++ lib/slack/sender.rb
@@ -8,26 +8,42 @@ module Slack
 
     def initialize(token: ENV["SLACK_BOT_TOKEN"])
       @token = token
+
+      raise "Missing Slack Bot Token" if @token.nil? || @token.empty?
     end
 
     def post_message(channel:, text:)
-      uri = URI(SLACK_API_URL)
+      request = build_request(channel: channel, text: text)
+      response = send_request(request)
+      validate_response!(response)
 
+      true
+    end
+
+    private
+
+    def build_request(channel:, text:)
+      uri = URI(SLACK_API_URL)
       req = Net::HTTP::Post.new(uri)
       req["Authorization"] = "Bearer #{@token}"
       req["Content-Type"] = "application/json"
       req.body = { channel: channel, text: text }.to_json
 
-      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      req
+    end
+
+    def send_request(req)
+      uri = URI(SLACK_API_URL)
+
+      Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
         http.request(req)
       end
+    end
 
+    def validate_response!(res)
       body = JSON.parse(res.body)
-      unless body["ok"]
-        raise "Slack API error: #{body["error"]}"
-      end
 
-      true
+      raise "Slack API error: #{body["error"]}" unless body["ok"]
     end
   end
 end
